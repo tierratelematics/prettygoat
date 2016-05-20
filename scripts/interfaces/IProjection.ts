e6198c311b36a9762f85e00b5cb6c9925e949414
Fabrizio Chignoli <chignoli@pobox.com>
2016-05-20 09:24:24 +0200

    Changed projection signature

--- scripts/interfaces/IProjection.ts
+++ scripts/interfaces/IProjection.ts
@@ -10,11 +10,21 @@ export class NamedStreamSource extends StreamSource { name: string; }
 
 export class MultipleStreamSource extends StreamSource { names: Array<string>; }
 
+export interface IWhen<T extends Object> {
+    $init?: () => T;
+    $any?: (s: T, e: Object) => T;
+    [name: string]: (s: T, e: Object) => T;
+}
+
+export interface ISplit {
+    $default?: (e: Object) => string;
+    [name: string]: (e: Object) => string;
+}
+
 export interface IProjection<T> {
     name: string;
-    splits: boolean;
-    splitKeys: any;
+    split?: ISplit;
     streamSource: StreamSource;
-    definition: any;
-    snapshotStrategy?(): ISnapshotStrategy;
+    definition: IWhen<T>;
+    snapshotStrategy?: ISnapshotStrategy;
 }
