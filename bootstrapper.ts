8b16c434b554808a01b2cdb0d78e870d6d300937
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-25 10:50:53 +0200

    Make project build

--- bootstrapper.ts
+++
@@ -1,9 +0,0 @@
-/*
-let engine = new ProjectionEngine();
-
-engine.register(new CountProjection());
-engine.register(new SimpleProjection());
-engine.register(new SplitProjection());
-
-engine.run();
-*/
