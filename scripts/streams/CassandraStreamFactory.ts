54c920b48d5532a0cda2b51e18b42620db15dde9
Mattia Franchetto <mt.franchetto@gmail.com>
2016-06-08 13:59:00 +0200

    Cleaner code

--- scripts/streams/CassandraStreamFactory.ts
+++ scripts/streams/CassandraStreamFactory.ts
@@ -6,7 +6,6 @@ import StreamState from "./StreamState";
 import ICassandraClientFactory from "./ICassandraClientFactory";
 import TimePartitioner from "./TimePartitioner";
 import * as Promise from "bluebird";
-import * as _ from "lodash";
 
 @injectable()
 class CassandraStreamFactory implements IStreamFactory {
@@ -29,23 +28,8 @@ class CassandraStreamFactory implements IStreamFactory {
     streamSource():Rx.Observable<any> {
         return Rx.Observable.create(observer => {
             Promise.resolve()
-                .then(() => {
-                    if (this.streamState.lastEvent)
-                        return this.timePartitioner.bucketsFrom(this.streamState.lastEvent.getDate());
-                    else
-                        return Promise.fromNode(callback => {
-                            this.client.execute("select distinct timebucket from event_by_timestamp", callback)
-                        }).then(buckets => buckets.rows).map<any, string>(row => row.timebucket);
-                })
-                .then(buckets => {
-                    let bucketsString = buckets.join("', '"),
-                        query = `SELECT blobAsText(event), timestamp FROM event_by_timestamp WHERE timebucket IN ('${bucketsString}')`;
-                    if (this.streamState.lastEvent) {
-                        let timestamp = this.streamState.lastEvent.getDate().toISOString();
-                        query += ` AND timestamp > maxTimeUuid('${timestamp}')`;
-                    }
-                    return query;
-                })
+                .then(() => this.getBuckets())
+                .then(buckets => this.buildQueryFromBuckets(buckets))
                 .then(query => {
                     this.client.stream(query)
                         .on('readable', function () {
@@ -65,6 +49,24 @@ class CassandraStreamFactory implements IStreamFactory {
         });
     }
 
+    private getBuckets():string[] | Promise<string[]> {
+        if (this.streamState.lastEvent)
+            return this.timePartitioner.bucketsFrom(this.streamState.lastEvent.getDate());
+        else
+            return Promise.fromNode(callback => {
+                this.client.execute("select distinct timebucket from event_by_timestamp", callback)
+            }).then(buckets => buckets.rows).map<any, string>(row => row.timebucket);
+    }
+
+    private buildQueryFromBuckets(buckets:string[]):string {
+        let bucketsString = buckets.join("', '"),
+            query = `SELECT blobAsText(event), timestamp FROM event_by_timestamp WHERE timebucket IN ('${bucketsString}')`;
+        if (this.streamState.lastEvent) {
+            let timestamp = this.streamState.lastEvent.getDate().toISOString();
+            query += ` AND timestamp > maxTimeUuid('${timestamp}')`;
+        }
+        return query;
+    }
 }
 
 export default CassandraStreamFactory
