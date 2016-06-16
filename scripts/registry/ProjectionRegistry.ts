bff16c5d28dcb5e3ab7be4fc9a64b6fb56d850e7
Mattia Franchetto <mt.franchetto@gmail.com>
2016-06-16 10:44:37 +0200

    Register projections in inversify also using area

--- scripts/registry/ProjectionRegistry.ts
+++ scripts/registry/ProjectionRegistry.ts
@@ -7,12 +7,17 @@ import {injectable, inject} from "inversify";
 import {ProjectionAnalyzer} from "../projections/ProjectionAnalyzer";
 import {INewable} from "inversify";
 import IObjectContainer from "../bootstrap/IObjectContainer";
+import * as _ from "lodash";
 
 @injectable()
 class ProjectionRegistry implements IProjectionRegistry {
 
     private registry:AreaRegistry[] = [];
-    private unregisteredEntries:RegistryEntry<any>[] = [];
+    private unregisteredEntries:{
+        ctor:INewable<IProjectionDefinition<any>>,
+        name:string,
+        parametersKey?:(parameters:any) => string
+    }[] = [];
 
     constructor(@inject("ProjectionAnalyzer") private analyzer:ProjectionAnalyzer,
                 @inject("IObjectContainer") private container:IObjectContainer) {
@@ -27,27 +32,30 @@ class ProjectionRegistry implements IProjectionRegistry {
         return this.add(constructor).forArea(Constants.INDEX_AREA);
     }
 
-    add<T>(constructor:INewable<IProjectionDefinition<T>>, parametersKey?: (parameters:any) => string):IProjectionRegistry {
+    add<T>(constructor:INewable<IProjectionDefinition<T>>, parametersKey?:(parameters:any) => string):IProjectionRegistry {
         let name = Reflect.getMetadata("prettygoat:projection", constructor);
         if (!name)
             throw new Error("Missing Projection decorator");
-        let projection = this.getDefinitionFromConstructor(constructor, name).define();
-        let validationErrors = this.analyzer.analyze(projection);
-        if (validationErrors.length > 0)
-            throw new Error(validationErrors[0]);
-        this.unregisteredEntries.push(new RegistryEntry<T>(projection, name, parametersKey));
+        this.unregisteredEntries.push({ctor: constructor, name: name, parametersKey: parametersKey});
         return this;
     }
 
-    private getDefinitionFromConstructor<T>(constructor:INewable<IProjectionDefinition<T>>, name:string):IProjectionDefinition<T> {
-        const key = `prettygoat:definitions:${name}`;
+    private getDefinitionFromConstructor<T>(constructor:INewable<IProjectionDefinition<T>>, area:string, name:string):IProjectionDefinition<T> {
+        const key = `prettygoat:definitions:${area}:${name}`;
         if (!this.container.contains(key))
             this.container.set(key, constructor);
         return this.container.get<IProjectionDefinition<T>>(key);
     }
 
     forArea(area:string):AreaRegistry {
-        let areaRegistry = new AreaRegistry(area, this.unregisteredEntries);
+        let entries = _.map(this.unregisteredEntries, entry => {
+            let projection = this.getDefinitionFromConstructor(entry.ctor, area, entry.name).define();
+            let validationErrors = this.analyzer.analyze(projection);
+            if (validationErrors.length > 0)
+                throw new Error(validationErrors[0]);
+            return new RegistryEntry(projection, entry.name, entry.parametersKey);
+        });
+        let areaRegistry = new AreaRegistry(area, entries);
         this.registry.push(areaRegistry);
         this.unregisteredEntries = [];
         return areaRegistry;
