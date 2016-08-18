43e4acff2d063b43aca72e16f57a0bbb40b60b6d
Claudio Iabichino <claudio.iabichino@synthesis3.it>
2016-08-18 17:44:37 +0200

    Export filter type definitions

--- scripts/prettygoat.ts
+++ scripts/prettygoat.ts
@@ -3,3 +3,4 @@ export {default as Projection} from "./registry/ProjectionDecorator";
 export {default as Engine} from "./bootstrap/Engine";
 export {default as TimeSnapshotStrategy} from "./snapshots/TimeSnapshotStrategy";
 export {default as CountSnapshotStrategy} from "./snapshots/CountSnapshotStrategy";
+export {default as FilterOutputType} from "./filters/FilterOutputType";
