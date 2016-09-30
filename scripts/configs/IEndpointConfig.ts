70f789353b355651d86ce1315fb5893158c53cda
Mattia Franchetto <mt.franchetto@gmail.com>
2016-09-30 10:49:40 +0200

    Configurable notifications url

--- scripts/configs/IEndpointConfig.ts
+++ scripts/configs/IEndpointConfig.ts
@@ -3,6 +3,12 @@ interface IEndpointConfig {
     port?:number;
     protocol:string;
     path?:string;
+    notifications?:{
+        host:string;
+        port?:number;
+        protocol:string;
+        path?:string;
+    }
 }
 
 export default IEndpointConfig
