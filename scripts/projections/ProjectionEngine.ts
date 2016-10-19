4c094d53ccaf8d5688554246e851b84c7fdda6d2
Mattia Franchetto <mt.franchetto@gmail.com>
2016-10-19 16:42:28 +0200

    Sample push to 500ms

--- scripts/projections/ProjectionEngine.ts
+++ scripts/projections/ProjectionEngine.ts
@@ -9,7 +9,6 @@ import IStatePublisher from "../routing/IStatePublisher";
 import {ISnapshotRepository, Snapshot} from "../snapshots/ISnapshotRepository";
 import RegistryEntry from "../registry/RegistryEntry";
 import IProjectionRunnerFactory from "./IProjectionRunnerFactory";
-import {Observable} from "rx";
 
 @injectable()
 class ProjectionEngine implements IProjectionEngine {
@@ -32,7 +31,7 @@ class ProjectionEngine implements IProjectionEngine {
                     _.forEach<RegistryEntry<any>>(areaRegistry.entries, (entry:RegistryEntry<any>) => {
                         let runner = this.runnerFactory.create(entry.projection),
                             context = new PushContext(areaRegistry.area, entry.name);
-                        runner.notifications().subscribe(state => {
+                        runner.notifications().sample(500).subscribe(state => {
                             let snapshotStrategy = entry.projection.snapshotStrategy;
                             this.pushNotifier.notify(context, null, state.splitKey);
                             if (snapshotStrategy && snapshotStrategy.needsSnapshot(state)) {
