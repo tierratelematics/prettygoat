5beae380abf8a6fea7bf0f41648a6cfd470ef463
Mattia Franchetto <mt.franchetto@gmail.com>
2016-10-19 12:19:45 +0200

    Fix memoizing matcher

--- test/MemoizingMatcherSpec.ts
+++ test/MemoizingMatcherSpec.ts
@@ -1,32 +1,38 @@
 import "bluebird";
 import "reflect-metadata";
-import {Matcher} from "../scripts/matcher/Matcher";
-
+import * as TypeMoq from "typemoq";
 import expect = require("expect.js");
 import {MemoizingMatcher} from "../scripts/matcher/MemoizingMatcher";
 import {IMatcher} from "../scripts/matcher/IMatcher";
+import {MockMatcher} from "./fixtures/MockMatcher";
 
 describe("Given a MemoizingMatcher", () => {
     let subject:IMatcher,
+        baseMatcher: TypeMoq.Mock<IMatcher>,
         testFn = (s, e) => s;
 
     describe("when matching an event", () => {
         beforeEach(() => {
-            subject = new MemoizingMatcher(new Matcher({"test": testFn}));
-        });
-
-        context("and an event of the same type has already been processed", () => {
-            it("should return the previous matching function", () => {
-                subject.match("test");
-                let match = subject.match("test");
-                expect(match).to.be(testFn);
-            });
+            baseMatcher = TypeMoq.Mock.ofType<IMatcher>(MockMatcher);
+            baseMatcher.setup(m => m.match("test")).returns(a => testFn);
+            subject = new MemoizingMatcher(baseMatcher.object);
         });
 
         context("and an event of the same type has not already been processed", () => {
             it("should return the previous matching function", () => {
                 let match = subject.match("test");
                 expect(match).to.be(testFn);
+                baseMatcher.verify(m => m.match("test"), TypeMoq.Times.once());
+            });
+        });
+
+        context("and an event of the same type has already been processed", () => {
+            it("should not call the base matcher anymore", () => {
+                subject.match("test");
+                baseMatcher.verify(m => m.match("test"), TypeMoq.Times.once());
+                let match = subject.match("test");
+                expect(match).to.be(testFn);
+                baseMatcher.verify(m => m.match("test"), TypeMoq.Times.once());
             });
         });
     });
