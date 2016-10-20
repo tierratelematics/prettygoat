1064f694eaf1d34766dc191f0742801b8d049f59
Mattia Franchetto <mt.franchetto@gmail.com>
2016-10-20 10:12:40 +0200

    Add logging classes

---
+++ scripts/log/NullLogger.ts
@@ -0,0 +1,23 @@
+import ILogger from "./ILogger";
+import LogLevel from "./LogLevel";
+
+class NullLogger implements ILogger {
+
+
+    debug(message:string) {
+    }
+
+    info(message:string) {
+    }
+
+    warning(message:string) {
+    }
+
+    error(error:string|Error) {
+    }
+
+    setLogLevel(level:LogLevel) {
+    }
+}
+
+export default NullLogger
