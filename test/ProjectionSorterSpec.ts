86e245c2b62c4290e82f2763fbc2152188d4900b
Mario Gualtieri <mario.gualtieri@3punto6.com>
2016-11-16 15:44:27 +0100

    refactor spec tester

--- test/ProjectionSorterSpec.ts
+++ test/ProjectionSorterSpec.ts
@@ -2,26 +2,11 @@ import "bluebird";
 import "reflect-metadata";
 import expect = require("expect.js");
 import sinon = require("sinon");
-import * as TypeMoq from "typemoq";
 import IProjectionRegistry from "../scripts/registry/IProjectionRegistry";
-import ProjectionRegistry from "../scripts/registry/ProjectionRegistry";
 import MockProjectionDefinition from "./fixtures/definitions/MockProjectionDefinition";
 import SinonStub = Sinon.SinonStub;
-import UnnamedProjectionDefinition from "./fixtures/definitions/UnnamedProjectionDefinition";
-import MockBadProjectionDefinition from "./fixtures/definitions/MockBadProjectionDefinition";
-import {ProjectionAnalyzer} from "../scripts/projections/ProjectionAnalyzer";
-import MockObjectContainer from "./fixtures/MockObjectContainer";
-import IProjectionRunner from "../scripts/projections/IProjectionRunner";
-import {ProjectionRunner} from "../scripts/projections/ProjectionRunner";
-import IObjectContainer from "../scripts/bootstrap/IObjectContainer";
-import IProjectionDefinition from "../scripts/registry/IProjectionDefinition";
-import ITickScheduler from "../scripts/ticks/ITickScheduler";
-import TickScheduler from "../scripts/ticks/TickScheduler";
-import Dictionary from "../scripts/Dictionary";
-import MockDateRetriever from "./fixtures/MockDateRetriever";
 import ProjectionSorter from "../scripts/projections/ProjectionSorter";
 import IProjectionSorter from "../scripts/projections/IProjectionSorter";
-import {IProjection} from "../scripts/projections/IProjection";
 import {
     MockProjectionCircularADefinition,
     MockProjectionCircularBDefinition
