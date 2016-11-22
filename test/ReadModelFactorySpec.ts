17d29cb1cb0a8d7fb9315c37864747b31188f4a6
Mario Gualtieri <mario.gualtieri@3punto6.com>
2016-11-22 16:17:39 +0100

    check event object inside ModelFactorySpec

--- test/ReadModelFactorySpec.ts
+++ test/ReadModelFactorySpec.ts
@@ -11,26 +11,26 @@ import {
 import MockProjectionRegistry from "./fixtures/MockProjectionRegistry";
 import RegistryEntry from "../scripts/registry/RegistryEntry";
 import ReadModelFactory from "../scripts/streams/ReadModelFactory";
+import {Event} from "../scripts/streams/Event";
 
 describe("Given a Read Model Factory", () => {
 
     let registry: TypeMoq.Mock<IProjectionRegistry>,
         subject: ReadModelFactory,
-        events:any[];
+        events:any[],
+        event:Event;
 
     beforeEach(() => {
         events = [];
         registry = TypeMoq.Mock.ofType(MockProjectionRegistry);
         subject = new ReadModelFactory(registry.object);
-        subject.publish({
-                type: "CircularA",
-                payload: {
-                    count: 20,
-                    id: "10"
-                },
-                timestamp: new Date(10), splitKey: null
-            }
-        );
+        event = {
+            type: "CircularA",
+            payload: null,
+            timestamp: new Date(10),
+            splitKey: null
+        };
+        subject.publish(event);
     });
 
     context("when a read model is handled by projection", () => {
@@ -46,6 +46,7 @@ describe("Given a Read Model Factory", () => {
         it("should not emit an event", () => {
             subject.from(null, new MockProjectionDefinition().define().definition).subscribe(event => events.push(event));
             expect(events).to.have.length(1);
+            expect(events[0]).to.be.eql(event);
         });
     });
 
