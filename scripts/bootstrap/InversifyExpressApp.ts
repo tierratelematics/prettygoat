e51939dffe83e628e6cbedb4a19dbbf46b9a4cc2
Mario Gualtieri <mario.gualtieri@3punto6.com>
2016-11-30 11:52:48 +0100

    initialize method inside SocketEventEmitter

--- scripts/bootstrap/InversifyExpressApp.ts
+++ scripts/bootstrap/InversifyExpressApp.ts
@@ -1,10 +1,11 @@
 import { InversifyExpressServer } from 'inversify-express-utils';
 
+const cors = require("cors");
 let server = null;
 
 export function createServer(kernel:any) {
     if(!server) {
-        server = new InversifyExpressServer(kernel).build();
+        server = new InversifyExpressServer(kernel).build().use(cors());
     }
     return server;
 }
--- scripts/push/SocketEventEmitter.ts
+++ scripts/push/SocketEventEmitter.ts
@@ -8,13 +8,19 @@ class SocketEventEmitter implements IEventEmitter {
 
     private socket:SocketIO.Server;
 
-    constructor(@inject("SocketFactory") socketFactory:SocketFactory,
-                @inject("ISocketConfig") socketConfig:ISocketConfig) {
-        this.socket = socketFactory.socketForPath(socketConfig.path);
+    constructor(@inject("SocketFactory") private socketFactory:SocketFactory,
+                @inject("ISocketConfig") private socketConfig:ISocketConfig) {
     }
 
     emitTo(clientId:string, event:string, parameters:any):void {
+        this.initialize();
         this.socket.to(clientId).emit(event, parameters);
+    }
+
+    private initialize(){
+        if(!this.socket){
+            this.socket = this.socketFactory.socketForPath(this.socketConfig.path);
+        }
     }
 
 }
