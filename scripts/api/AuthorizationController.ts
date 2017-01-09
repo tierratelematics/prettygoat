536f7522770cc66906fe2bb209f57842e81da738
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-01-09 11:58:55 +0100

    Add engineData inside AuthorizationController

--- scripts/api/AuthorizationController.ts
+++ scripts/api/AuthorizationController.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
-import {injectable} from 'inversify';
-import {interfaces,Controller, Post} from 'inversify-express-utils';
+import {injectable, inject} from 'inversify';
+import {interfaces, Controller, Post} from 'inversify-express-utils';
+import IEngineData from "../configs/IEngineData";
 
 @Controller('/api/authorization')
 @injectable()
 class AuthorizationController implements interfaces.Controller {
 
+    constructor(@inject("IEngineData") private engineData: IEngineData) {
+    }
+
     @Post('/check')
     check(request: express.Request, response: express.Response): void {
-        response.json({authorization: "success"});
+        response.json({authorization: "success", engineData: this.engineData});
     }
 
 }
