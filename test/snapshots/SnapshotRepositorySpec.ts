951b47d32dbba788f769b0ed84039e5ef61dc69b
Mattia Franchetto <mt.franchetto@gmail.com>
2017-01-09 15:34:03 +0100

    Replace single quotes without double quotation marks

--- test/snapshots/SnapshotRepositorySpec.ts
+++ test/snapshots/SnapshotRepositorySpec.ts
@@ -82,7 +82,13 @@ describe("Snapshot repository, given all the streams", () => {
                 let snapshot = new Snapshot({a: 25}, new Date(500));
                 subject.saveSnapshot("test", snapshot);
                 cassandraClient.verify(c => c.execute(`insert into projections_snapshots (streamid, split, lastevent, memento) values ('test',` +
-                    `'', '${snapshot.lastEvent}', textAsBlob('${JSON.stringify(snapshot.memento)}'))`), TypeMoq.Times.once());
+                    `'', '${snapshot.lastEvent}', textAsBlob('{"a":25}'))`), TypeMoq.Times.once());
+            });
+            it("should escape single quotes correctly", () => {
+                let snapshot = new Snapshot({a: "'"}, new Date(500));
+                subject.saveSnapshot("test", snapshot);
+                cassandraClient.verify(c => c.execute(`insert into projections_snapshots (streamid, split, lastevent, memento) values ('test',` +
+                    `'', '${snapshot.lastEvent}', textAsBlob('{"a":"''"}'))`), TypeMoq.Times.once());
             });
         });
 
