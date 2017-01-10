492ed8c146fb06731ca9f793924b3312486f8bb3
Mattia Franchetto <mt.franchetto@gmail.com>
2017-01-10 18:23:25 +0100

    Add more test to cluster spec

--- test/cluster/ClusterSpec.ts
+++ test/cluster/ClusterSpec.ts
@@ -1,8 +1,15 @@
+import "reflect-metadata";
+import expect = require("expect.js");
+
 describe("Given a set of nodes", () => {
     context("when a node starts", () => {
         it("should run the projections that match", () => {
 
         });
+    });
+
+    context("when a projection running on a node dies", () => {
+       it("should restart the projection");
     });
 
     context("when the nodes ring is changed", () => {
--- test/cluster/RedistributionSpec.ts
+++ test/cluster/RedistributionSpec.ts
@@ -1,3 +1,6 @@
+import "reflect-metadata";
+import expect = require("expect.js");
+
 describe("Given a set of projections to redistribute", () => {
     context("when a projection is assigned to a node", () => {
         context("and it was already running", () => {
@@ -6,5 +9,9 @@ describe("Given a set of projections to redistribute", () => {
         context("and it was not running", () => {
             it("should run that projection");
         });
+    });
+    context("when a projection is not assigned anymore to a certain node", () => {
+       it("should be shut down");
+       it("should snapshot before dying");
     });
 });
