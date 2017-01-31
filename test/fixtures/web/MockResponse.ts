f79fd706abb40a2f2a57a03c3871c8634165f46f
Mattia Franchetto <mt.franchetto@gmail.com>
2017-01-31 11:31:38 +0100

    Make test pass

--- test/fixtures/web/MockResponse.ts
+++ test/fixtures/web/MockResponse.ts
@@ -1,6 +1,11 @@
 import {IResponse} from "../../../scripts/web/IRequestComponents";
 
 export default class MockResponse implements IResponse {
+    setHeader(key: string, value: string) {
+    }
+
+    end() {
+    }
     originalResponse: null;
 
     header(key: string, value: string) {
