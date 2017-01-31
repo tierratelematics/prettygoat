f6b8553c0f3ac8ff13fb89049c35ddb2b2b75721
Mattia Franchetto <mt.franchetto@gmail.com>
2017-01-31 15:22:07 +0100

    Add default poll to push config

--- scripts/streams/PollToPushStreamFactory.ts
+++ scripts/streams/PollToPushStreamFactory.ts
@@ -1,16 +1,16 @@
 import {IStreamFactory} from "./IStreamFactory";
-import {injectable, inject} from "inversify";
-import IPollToPushConfig from "../configs/IPollToPushConfig";
-import {Scheduler, Observable} from "rx";
+import {injectable, inject, optional} from "inversify";
+import {Observable} from "rx";
 import {Event} from "./Event";
 import {IWhen} from "../projections/IProjection";
 import ReservedEvents from "../streams/ReservedEvents";
+import {DefaultPollToPushConfig, IPollToPushConfig} from "../configs/PollToPushConfig";
 
 @injectable()
 class PollToPushStreamFactory implements IStreamFactory {
 
     constructor(@inject("StreamFactory") private streamFactory:IStreamFactory,
-                @inject("IPollToPushConfig") private config:IPollToPushConfig) {
+                @inject("IPollToPushConfig") @optional() private config:IPollToPushConfig = new DefaultPollToPushConfig()) {
 
     }
 
