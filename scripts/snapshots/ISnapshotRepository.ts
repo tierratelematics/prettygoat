9d4bb8d8343c98f151414fd382c7243b12765e7b
Mattia Franchetto <mt.franchetto@gmail.com>
2017-02-09 18:31:04 +0100

    Reformat code

--- scripts/snapshots/ISnapshotRepository.ts
+++ scripts/snapshots/ISnapshotRepository.ts
@@ -2,16 +2,16 @@ import {Observable} from "rx";
 import Dictionary from "../util/Dictionary";
 
 export class Snapshot<T> {
-    public static Empty:Snapshot<any> = new Snapshot<any>(undefined, undefined);
+    public static Empty: Snapshot<any> = new Snapshot<any>(undefined, undefined);
 
-    constructor(public memento:T, public lastEvent:Date) {
+    constructor(public memento: T, public lastEvent: Date) {
     }
 }
 
 export interface ISnapshotRepository {
-    initialize():Observable<void>;
-    getSnapshots():Observable<Dictionary<Snapshot<any>>>;
-    getSnapshot<T>(streamId:string):Observable<Snapshot<T>>;
-    saveSnapshot<T>(streamId:string, snapshot:Snapshot<T>):void;
-    deleteSnapshot(streamId:string):Observable<void>;
+    initialize(): Observable<void>;
+    getSnapshots(): Observable<Dictionary<Snapshot<any>>>;
+    getSnapshot<T>(streamId: string): Observable<Snapshot<T>>;
+    saveSnapshot<T>(streamId: string, snapshot: Snapshot<T>): void;
+    deleteSnapshot(streamId: string): Observable<void>;
 }
