5f16c4a5ea409ac27392d024520f6142b36212aa
Mattia Franchetto <mt.franchetto@gmail.com>
2017-02-17 14:29:37 +0100

    Reformat code

--- scripts/configs/ICassandraConfig.ts
+++ scripts/configs/ICassandraConfig.ts
@@ -1,8 +1,8 @@
 interface ICassandraConfig {
-    hosts:string[];
-    keyspace:string;
-    fetchSize?:number;
-    readDelay?:number;
+    hosts: string[];
+    keyspace: string;
+    fetchSize?: number;
+    readDelay?: number;
 }
 
 export default ICassandraConfig
