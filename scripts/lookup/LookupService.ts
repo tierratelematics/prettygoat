f28cc25755aac17dc7cbe52b617eff49fd019422
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-15 17:19:29 +0200

    Cache lookups

--- scripts/lookup/LookupService.ts
+++ scripts/lookup/LookupService.ts
@@ -1,6 +1,7 @@
 import {injectable, inject} from "inversify";
 import IProjectionRunner from "../projections/IProjectionRunner";
 import Dictionary from "../util/Dictionary";
+import Lookup from "./Lookup";
 
 export interface ILookupService {
     keysFor(id: string, projectionName: string): Promise<string[]>;
@@ -9,12 +10,22 @@ export interface ILookupService {
 @injectable()
 export class LookupService implements ILookupService {
 
+    private cache: Dictionary<Dictionary<Lookup>> = {};
+
     constructor(@inject("IProjectionRunnerHolder") private runners: Dictionary<IProjectionRunner<any>>) {
 
     }
 
     keysFor(id: string, projectionName: string): Promise<string[]> {
-        return this.runners[projectionName].notifications().map(notification => notification.payload[id]).toPromise<Promise<string[]>>(Promise);
+        this.cache[projectionName] = this.cache[projectionName] || {};
+        let cached = this.cache[projectionName][id];
+        if (cached)
+            return Promise.resolve(cached);
+        else
+            return this.runners[projectionName].notifications()
+                .map(notification => notification.payload[id])
+                .do(value => this.cache[projectionName][id] = value)
+                .toPromise<Promise<string[]>>(Promise);
     }
 
 }
