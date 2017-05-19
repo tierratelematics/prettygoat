34c65471efa97efce4533a584df155ccc9eabcf9
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-19 13:57:04 +0200

    Align time tick spec

--- test/fixtures/PublishProjectionRunner.ts
+++ test/fixtures/PublishProjectionRunner.ts
@@ -1,16 +1,15 @@
 import ProjectionRunner from "../../scripts/projections/ProjectionRunner";
-import IDateRetriever from "../../scripts/util/IDateRetriever";
-import {IStreamFactory} from "../../scripts/streams/IStreamFactory";
 import IReadModelFactory from "../../scripts/streams/IReadModelFactory";
 import {IMatcher} from "../../scripts/matcher/IMatcher";
 import {IProjection} from "../../scripts/projections/IProjection";
 import {Subject} from "rx";
+import {IProjectionStreamGenerator} from "../../scripts/projections/ProjectionStreamGenerator";
 
 export default class PublishProjectionRunner<T> extends ProjectionRunner<T> {
 
-    constructor(protected projection: IProjection<T>, protected stream: IStreamFactory, protected matcher: IMatcher, protected readModelFactory: IReadModelFactory,
-                protected tickScheduler: IStreamFactory, protected dateRetriever: IDateRetriever, protected realtimeNotifier: Subject<string>) {
-        super(projection, stream, matcher, readModelFactory, tickScheduler, dateRetriever, realtimeNotifier);
+    constructor(protected projection: IProjection<T>, protected stream: IProjectionStreamGenerator, protected matcher: IMatcher,
+                protected readModelFactory: IReadModelFactory, protected realtimeNotifier: Subject<string>) {
+        super(projection, stream, matcher, readModelFactory, realtimeNotifier);
     }
 
 
