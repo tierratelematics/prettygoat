0bfb76311760efb010ea593106105b449c61bf9f
Mattia Franchetto <mt.franchetto@gmail.com>
2017-06-22 14:47:29 +0200

    Started refactoring readmodels

--- scripts/streams/IReadModel.ts
+++
@@ -1,9 +0,0 @@
-import {Event} from "./Event";
-
-export interface IReadModelPublisher {
-    publish(event: Event);
-}
-
-export interface IReadModelProvider {
-    modelFor<T>(projectionName: string): Promise<T>;
-}
