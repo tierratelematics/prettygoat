cce2a2ece08c25099cca3a415cb8496402a281f1
Mattia Franchetto <mt.franchetto@gmail.com>
2017-07-03 18:57:39 +0200

    Fix stackoverflow on async handlers

--- scripts/util/RxOperators.ts
+++ scripts/util/RxOperators.ts
@@ -1,9 +1,9 @@
-import {Observable} from "rx";
+import {Observable, Scheduler} from "rx";
 import {ObservableOrPromise} from "./TypesUtil";
 
 export function flatMapSeries<T, T1>(selector: (data: T) => ObservableOrPromise<T1>) {
     return function (observable: Observable<T>) {
-        return observable.flatMapWithMaxConcurrent(1, data => Observable.defer(() => selector(data)));
+        return observable.flatMapWithMaxConcurrent(1, data => Observable.defer(() => selector(data)).observeOn(Scheduler.currentThread));
     }
 }
 
