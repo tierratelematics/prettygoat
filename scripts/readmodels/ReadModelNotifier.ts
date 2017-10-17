8d2ce5ddee216466640a4e9fab25704ee2d60bf0
Mattia Franchetto <mt.franchetto@gmail.com>
2017-10-17 16:00:51 +0200

    Add log for readmodel

--- scripts/readmodels/ReadModelNotifier.ts
+++ scripts/readmodels/ReadModelNotifier.ts
@@ -1,7 +1,8 @@
 import {Event} from "../events/Event";
 import {Observable, Subject} from "rxjs";
 import SpecialEvents from "../events/SpecialEvents";
-import {injectable} from "inversify";
+import {inject, injectable} from "inversify";
+import {ILogger, NullLogger, LoggingContext} from "inversify-logging";
 
 export interface IReadModelNotifier {
     changes(name: string): Observable<Event>;
@@ -10,8 +11,10 @@ export interface IReadModelNotifier {
 }
 
 @injectable()
+@LoggingContext("ReadModelNotifier")
 export class ReadModelNotifier implements IReadModelNotifier {
 
+    @inject("ILogger") private logger: ILogger = NullLogger;
     private subject = new Subject<Event>();
 
     changes(name: string): Observable<Event> {
@@ -19,6 +22,8 @@ export class ReadModelNotifier implements IReadModelNotifier {
     }
 
     notifyChanged(name: string, timestamp: Date, eventId?: string) {
+        let logger = this.logger.createChildLogger(name);
+        logger.debug(`Readmodel is changed at timestamp ${timestamp}`);
         this.subject.next({
             type: SpecialEvents.READMODEL_CHANGED,
             payload: name,
