0af319565b7f78de67578bca788ad5b5e20306eb
Mattia Franchetto <mt.franchetto@gmail.com>
2017-11-17 11:10:28 +0100

    Expose memento producer

--- scripts/snapshots/MementoProducer.ts
+++ scripts/snapshots/MementoProducer.ts
@@ -1,9 +1,11 @@
 import { Event } from "../events/Event";
+import { injectable } from "inversify";
 
 export interface IMementoProducer<T> {
     produce(event: Event): T;
 }
 
+@injectable()
 export class MementoProducer implements IMementoProducer<any> {
 
     produce(event: Event<any>): any {
